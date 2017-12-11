82b4220a7d2be0dad2e78b81474456fa7a921a8c
Akshay Dajee <adajee@gmail.com>
2017-12-10 20:04:41 -0500

    aliens

---
+++ Hello Aliens.R
@@ -0,0 +1,22 @@
+
+cat("Hello, My name is Akshay Dajee and I am currently a Senior in the Tepper School of Business 
+at Carnegie Mellon University. I am double majoring in Finance and Statistics. 
+Recently, I accepted an offer to work for an investment bank called Cowen where I will
+be working in the M&A division in the Healthcare group. I have been selected to talk 
+to you about Earth. So let me go over a couple of key things. There are 7 continents, lots of
+countries. While you are here you should visit some. Spain is my favorite country. I am grouping
+there over Winter Break. You guys should also go to the movie theater while you are here. 
+Humans like to make these 2 hour motion pictures that they called movies. They are pretty cool.
+The new avengers movie is playing, I heard it is good. Another thing you should do is 
+go to New York and eat some hot dogs or italian sausages. Also pizza. Great food.I would love to
+learn more about you and your people. What do you call yourselves? Do you guys have multiple
+languages you guys speak?")
+
+
+
+
+
+
+
+
+
